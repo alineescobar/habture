f9ccb288a8dffd8038bf02fbae2c62b8a08c3628
Aline Escobar <aline.escobar@outlook.com>
2021-05-06 15:34:25 -0300

    Initial Commit

---
+++ habture/habtureApp.swift
@@ -0,0 +1,17 @@
+//
+//  habtureApp.swift
+//  habture
+//
+//  Created by Aline Osana Escobar on 06/05/21.
+//
+
+import SwiftUI
+
+@main
+struct habtureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
