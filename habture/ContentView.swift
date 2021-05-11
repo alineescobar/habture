dda912996bd4c986ec62225df2038ca37919e180
Aline Escobar <aline.escobar@outlook.com>
2021-05-11 18:26:25 -0300

    feat: personalized tab bar

--- habture/ContentView.swift
+++ habture/ContentView.swift
@@ -8,14 +8,122 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        Text("Hello, world!")
-            .padding()
+    @StateObject var viewRouter: ViewRouter
+    var body: some View{
+        GeometryReader { geometry in
+            VStack {
+                Spacer()
+                switch viewRouter.currentPage {
+                case .categorias:
+                    Text("CATEGORIAS")
+                case .conquistas:
+                    Text("CONQUISTAS")
+                case .ajustes:
+                    Text("AJUSTES")
+                case .perfil:
+                    Text("PERFIL")
+                }
+                Spacer()
+                HStack(alignment: .lastTextBaseline){
+                    HStack{
+                        TabBarIcon(
+                            viewRouter: viewRouter,
+                            assignedPage: Page.categorias,
+                            width: 28,
+                            height: 28,
+                            iconName: "camera.filters",
+                            tabName: "Categorias"
+                        )
+                        .padding(EdgeInsets(top: 0, leading: 30, bottom: 0, trailing: 13))
+                        
+                        TabBarIcon(
+                            viewRouter: viewRouter,
+                            assignedPage: Page.conquistas,
+                            width: 20,
+                            height: 28,
+                            iconName: "rosette",
+                            tabName: "Conquistas"
+                        )
+                        .padding(EdgeInsets(top: 0, leading: 13, bottom: 0, trailing: 13))
+
+                        TabBarIcon(
+                            viewRouter: viewRouter,
+                            assignedPage: Page.ajustes,
+                            width: 28,
+                            height: 28,
+                            iconName: "gearshape",
+                            tabName: "Ajustes"
+                        )
+                        .padding(EdgeInsets(top: 0, leading: 13, bottom: 0, trailing: 13))
+
+                        TabBarIcon(
+                            viewRouter: viewRouter,
+                            assignedPage: Page.perfil,
+                            width: 28,
+                            height: 28,
+                            iconName: "person",
+                            tabName: "Perfil"
+                        )
+                        .padding(EdgeInsets(top: 0, leading: 13, bottom: 0, trailing: 30))
+                    }
+
+                }
+                .frame(width: geometry.size.width, height: geometry.size.height/9)
+                .background(Color("TabBarPurple"))
+                .cornerRadius(28, corners: [.topLeft, .topRight])
+            }
+            .ignoresSafeArea()
+        }
+    }
+}
+
+extension View {
+    func cornerRadius(_ radius: CGFloat, corners: UIRectCorner) -> some View {
+        clipShape( RoundedCorner(radius: radius, corners: corners) )
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ContentView(viewRouter: ViewRouter())
+    }
+}
+
+struct TabBarIcon: View {
+    @StateObject var viewRouter: ViewRouter
+    let assignedPage: Page
+    
+    let width, height: CGFloat
+    let iconName, tabName: String
+    
+    var body: some View {
+        VStack{
+            Image(systemName: iconName)
+                .resizable()
+                .frame(width: width, height: height)
+                .padding(.top, 10)
+                .foregroundColor(viewRouter.currentPage == assignedPage ? Color("TabBarWhite") : Color("TabBarWhiteFadded"))
+            Text(tabName)
+                .font(.system(size: 10, design: .rounded))
+                .fontWeight(.semibold)
+                .frame(minWidth: 55)
+                .foregroundColor(viewRouter.currentPage == assignedPage ? Color("TabBarWhite") : Color("TabBarWhiteFadded"))
+            Spacer()
+        }
+        .frame(maxWidth: .infinity )
+        .onTapGesture {
+            viewRouter.currentPage = assignedPage
+        }
+        
+    }
+}
+
+struct RoundedCorner: Shape {
+    var radius: CGFloat = .infinity
+    var corners: UIRectCorner = .allCorners
+
+    func path(in rect: CGRect) -> Path {
+        let path = UIBezierPath(roundedRect: rect, byRoundingCorners: corners, cornerRadii: CGSize(width: radius, height: radius))
+        return Path(path.cgPath)
     }
 }
