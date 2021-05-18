3554462e3ee523b9fb1e5323302f3d3d6bd1c1eb
Igor Vicente <Igor.Vicente@edu.pucrs.br>
2021-05-18 14:16:38 -0400

    Ajustes de Design

--- habture/View/Awards.swift
+++ habture/View/Awards.swift
@@ -11,56 +11,33 @@ struct Awards: View {
     var body: some View {
         VStack {
             Spacer()
-                .frame(height: 111)
+                .frame(height: 70)
+            
             Text("Conquistas")
                 .fontWeight(.semibold)
-                .font(.system(size: 28, design: .rounded))
-                .foregroundColor(Color("Brown"))
-                .padding(.bottom)
+                .font(.system(size: 22, design: .rounded))
+                .foregroundColor(Color("Purple2"))
             
-            ZStack {
-                Rectangle()
-                    .fill(Color("Brown"))
-                    .border(Color.black, width: 15)
-                    .cornerRadius(20)
-                    .frame(width: .infinity, height: 450, alignment: .center)
+            ScrollView(showsIndicators: false) {
+                
+                Spacer()
+                    .frame(height: 51)
+                
+                RoundedRectangle(cornerRadius: 22, style: .continuous)
+                    .stroke(Color("Purple2"), lineWidth: 2)
+                    .frame(height: 181)
                     .overlay(
-                        VStack {
-                            HStack {
-                                RoundedRectangle(cornerRadius: 12, style: .continuous)
-                                    .fill(Color.gray)
-                                    .frame(width: 70, height: 70)
-                                    .overlay(
-                                        Image(systemName: "rosette")
-                                            .font(.title)
-                                            .foregroundColor(.white)
-                                    )
-                                    .shadow(color: .black, radius: 5, x: 5, y: 5)
-                                    .padding(10)
-                                
-                                RoundedRectangle(cornerRadius: 12, style: .continuous)
-                                    .fill(Color.gray)
-                                    .frame(width: 70, height: 70)
-                                    .overlay(
-                                        Image(systemName: "rosette")
-                                            .font(.title)
-                                            .foregroundColor(.white)
-                                    )
-                                    .shadow(color: .black, radius: 5, x: 5, y: 5)
-                                    .padding(10)
-                                
-                                RoundedRectangle(cornerRadius: 12, style: .continuous)
-                                    .fill(Color.gray)
-                                    .frame(width: 70, height: 70)
-                                    .overlay(
-                                        Image(systemName: "rosette")
-                                            .font(.title)
-                                            .foregroundColor(.white)
-                                    )
-                                    .shadow(color: .black, radius: 5, x: 5, y: 5)
-                                    .padding(10)
-                                
-                            }
+                        ZStack {
+                            RoundedRectangle(cornerRadius: 22, style: .continuous)
+                                .fill(Color("Purple2"))
+                                .frame(width:257,height: 41)
+                                .overlay(
+                                    Text("Destino do lixo")
+                                        .fontWeight(.bold)
+                                        .font(.system(size: 18, design: .rounded))
+                                        .foregroundColor(Color("BackgroundColor"))
+                                )
+                                .offset(y: -90.0)
                             
                             HStack {
                                 RoundedRectangle(cornerRadius: 12, style: .continuous)
@@ -96,90 +73,88 @@ struct Awards: View {
                                     .shadow(color: .black, radius: 5, x: 5, y: 5)
                                     .padding(10)
                                 
-                            }
-                            
-                            HStack {
-                                RoundedRectangle(cornerRadius: 12, style: .continuous)
-                                    .fill(Color.gray)
-                                    .frame(width: 70, height: 70)
-                                    .overlay(
-                                        Image(systemName: "rosette")
-                                            .font(.title)
-                                            .foregroundColor(.white)
-                                    )
-                                    .shadow(color: .black, radius: 5, x: 5, y: 5)
-                                    .padding(10)
-                                
-                                RoundedRectangle(cornerRadius: 12, style: .continuous)
-                                    .fill(Color.gray)
-                                    .frame(width: 70, height: 70)
-                                    .overlay(
-                                        Image(systemName: "rosette")
-                                            .font(.title)
-                                            .foregroundColor(.white)
-                                    )
-                                    .shadow(color: .black, radius: 5, x: 5, y: 5)
-                                    .padding(10)
-                                
-                                RoundedRectangle(cornerRadius: 12, style: .continuous)
-                                    .fill(Color.gray)
-                                    .frame(width: 70, height: 70)
-                                    .overlay(
-                                        Image(systemName: "rosette")
-                                            .font(.title)
-                                            .foregroundColor(.white)
-                                    )
-                                    .shadow(color: .black, radius: 5, x: 5, y: 5)
-                                    .padding(10)
-                                
-                            }
-                            
-                            HStack {
-                                RoundedRectangle(cornerRadius: 12, style: .continuous)
-                                    .fill(Color.gray)
-                                    .frame(width: 70, height: 70)
-                                    .overlay(
-                                        Image(systemName: "rosette")
-                                            .font(.title)
-                                            .foregroundColor(.white)
-                                    )
-                                    .shadow(color: .black, radius: 5, x: 5, y: 5)
-                                    .padding(10)
-                                
-                                RoundedRectangle(cornerRadius: 12, style: .continuous)
-                                    .fill(Color.gray)
-                                    .frame(width: 70, height: 70)
-                                    .overlay(
-                                        Image(systemName: "rosette")
-                                            .font(.title)
-                                            .foregroundColor(.white)
-                                    )
-                                    .shadow(color: .black, radius: 5, x: 5, y: 5)
-                                    .padding(10)
-                                
-                                RoundedRectangle(cornerRadius: 12, style: .continuous)
-                                    .fill(Color.gray)
-                                    .frame(width: 70, height: 70)
-                                    .overlay(
-                                        Image(systemName: "rosette")
-                                            .font(.title)
-                                            .foregroundColor(.white)
-                                    )
-                                    .shadow(color: .black, radius: 5, x: 5, y: 5)
-                                    .padding(10)
                             }
                         }
-                        .padding(10)
                     )
-                    .padding(30)
+                    .padding(.horizontal,20)
+                
+                Spacer()
+                    .frame(height: 51)
+                
+                RoundedRectangle(cornerRadius: 22, style: .continuous)
+                    .stroke(Color("Orange1"), lineWidth: 2)
+                    .frame(height: 181)
+                    .overlay(
+                        ZStack {
+                            RoundedRectangle(cornerRadius: 22, style: .continuous)
+                                .fill(Color("Orange1"))
+                                .frame(width:257,height: 41)
+                                .overlay(
+                                    Text("Alimentação consciente")
+                                        .fontWeight(.bold)
+                                        .font(.system(size: 18, design: .rounded))
+                                        .foregroundColor(Color("BackgroundColor"))
+                                )
+                                .offset(y: -90.0)
+                        }
+                    )
+                    .padding(.horizontal,20)
+                
+                Spacer()
+                    .frame(height: 51)
+                
+                RoundedRectangle(cornerRadius: 22, style: .continuous)
+                    .stroke(Color("Blue"), lineWidth: 2)
+                    .frame(height: 181)
+                    .overlay(
+                        ZStack {
+                            RoundedRectangle(cornerRadius: 22, style: .continuous)
+                                .fill(Color("Blue"))
+                                .frame(width:257,height: 41)
+                                .overlay(
+                                    Text("Consumo sustentável")
+                                        .fontWeight(.bold)
+                                        .font(.system(size: 18, design: .rounded))
+                                        .foregroundColor(Color("BackgroundColor"))
+                                )
+                                .offset(y: -90.0)
+                        }
+                    )
+                    .padding(.horizontal,20)
+                
+                Spacer()
+                    .frame(height: 51)
+                
+                RoundedRectangle(cornerRadius: 22, style: .continuous)
+                    .stroke(Color("Pink"), lineWidth: 2)
+                    .frame(height: 181)
+                    .overlay(
+                        ZStack {
+                            RoundedRectangle(cornerRadius: 22, style: .continuous)
+                                .fill(Color("Pink"))
+                                .frame(width:257,height: 41)
+                                .overlay(
+                                    Text("Consumo sustentável")
+                                        .fontWeight(.bold)
+                                        .font(.system(size: 18, design: .rounded))
+                                        .foregroundColor(Color("BackgroundColor"))
+                                )
+                                .offset(y: -90.0)
+                        }
+                    )
+                    .padding(.horizontal,20)
+                
+                Spacer()
+                    .frame(height: 85)
             }
-            Spacer()
         }
     }
 }
 
 struct Awards_Previews: PreviewProvider {
     static var previews: some View {
-        Awards()
+        Group {
+            Awards()
+        }
     }
 }
